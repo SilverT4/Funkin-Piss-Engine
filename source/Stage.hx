d1f4e6626241c64a5c72476c0c57fe0739d9b1ae
Paidyy <wieszielonagora2@gmail.com>
2022-03-19 13:42:06 +0100

    moved background dancer to stage class

--- source/Stage.hx
+++ source/Stage.hx
@@ -608,3 +608,26 @@ class StageAsset extends FlxSprite {
         return this;
     }
 }
+
+class BackgroundDancer extends StageAsset {
+	public function new(x:Float, y:Float, dancerID:Int) {
+		super(x, y, "limoDancer" + dancerID);
+
+		frames = Paths.stageSparrow("limoDancer");
+		animation.addByIndices('danceLeft', 'bg dancer sketch PINK', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+		animation.addByIndices('danceRight', 'bg dancer sketch PINK', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+		animation.play('danceLeft');
+		antialiasing = true;
+	}
+
+	var danceDir:Bool = false;
+
+	public function dance():Void {
+		danceDir = !danceDir;
+
+		if (danceDir)
+			animation.play('danceRight', true);
+		else
+			animation.play('danceLeft', true);
+	}
+}

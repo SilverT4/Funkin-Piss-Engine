12b8c43adbc6d0db3e45763e0443d0156c578213
Paidyy <wieszielonagora2@gmail.com>
2022-03-27 16:56:00 +0200

    add crashhandler

--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,10 @@
 package;
 
+import OptionsSubState.Background;
+import clipboard.Clipboard;
+import flixel.text.FlxText;
+import haxe.Exception;
+import flixel.util.FlxColor;
 import openfl.display.StageDisplayState;
 import openfl.display.StageQuality;
 import openfl.filters.ShaderFilter;
@@ -70,10 +75,131 @@ class Main extends Sprite {
 		initialState = TitleState;
 		#end
 
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, Options.framerate, Options.framerate, skipSplash, startFullscreen));
+		addChild(new Game(gameWidth, gameHeight, initialState, zoom, Options.framerate, Options.framerate, skipSplash, startFullscreen));
 
 		#if !mobile
-		addChild(new FPS(10, 3, 0xFFFFFF));
+		addChild(new EFPS());
 		#end
+
+	}
+}
+
+class Game extends FlxGame {
+	override public function update() {
+		if (Options.disableCrashHandler) {
+			super.update();
+		}
+		else {
+			try {
+				super.update();
+			}
+			catch (exc) {
+				FlxG.switchState(new CrashHandler(exc));
+			}
+		}
+	}
+}
+
+class CrashHandler extends FlxState {
+	// inspired from ddlc mod and old minecraft crash handler
+	var exception:Exception;
+	var gf:Character;
+
+	public function new(exc:Exception) {
+		super();
+
+		exception = exc;
+	}
+
+	override function create() {
+		super.create();
+
+		var bg = new Background(FlxColor.fromString("#696969"));
+		bg.scrollFactor.set(0, 0);
+		add(bg);
+
+		var bottomText = new FlxText(0, 0, 0, "C to copy exception | ESC to send to menu");
+		bottomText.scrollFactor.set(0, 0);
+		bottomText.setFormat(Paths.font("vcr.ttf"), 12, FlxColor.WHITE);
+		bottomText.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 1);
+		bottomText.screenCenter(X);
+		bottomText.y = FlxG.height - bottomText.height - 10;
+		add(bottomText);
+
+		var exceptionText = new FlxText();
+		exceptionText.setFormat(Paths.font("vcr.ttf"), 42, FlxColor.WHITE);
+		exceptionText.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 1);
+		exceptionText.text = "Game has encountered a Exception!";
+		exceptionText.color = FlxColor.RED;
+		exceptionText.screenCenter(X);
+		exceptionText.y += 20;
+		add(exceptionText);
+
+		var crashShit = new FlxText();
+		crashShit.setFormat(Paths.font("vcr.ttf"), 15, FlxColor.WHITE);
+		crashShit.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 1);
+		crashShit.text = exception.details();
+		crashShit.screenCenter(X);
+		crashShit.y += exceptionText.y + exceptionText.height + 20;
+		add(crashShit);
+
+		gf = new Character(0, 0, "gf", false, true);
+		gf.scrollFactor.set(0, 0);
+		gf.animation.play("sad");
+		add(gf);
+
+		gf.setGraphicSize(Std.int(gf.frameWidth * 0.3));
+		gf.updateHitbox();
+		gf.x = FlxG.width - gf.width;
+		gf.y = FlxG.height - gf.height;
+	}
+
+	override function update(elapsed) {
+		if (FlxG.keys.justPressed.C) {
+			Clipboard.set(exception.details());
+		}
+
+		if (FlxG.keys.justPressed.ESCAPE)
+			FlxG.switchState(new MainMenuState());
+
+		if (FlxG.mouse.wheel == -1) {
+			if (FlxG.keys.pressed.CONTROL)
+				FlxG.camera.zoom += 0.02;
+			if (FlxG.keys.pressed.SHIFT)
+				FlxG.camera.scroll.x += 20;
+			if (!FlxG.keys.pressed.SHIFT && !FlxG.keys.pressed.CONTROL)
+				FlxG.camera.scroll.y += 20;
+		}
+		if (FlxG.mouse.wheel == 1) {
+			if (FlxG.keys.pressed.CONTROL)
+				FlxG.camera.zoom -= 0.02;
+			if (FlxG.keys.pressed.SHIFT) {
+				if (FlxG.camera.scroll.x > 0)
+					FlxG.camera.scroll.x -= 20;
+			}
+			if (!FlxG.keys.pressed.SHIFT && !FlxG.keys.pressed.CONTROL) {
+				if (FlxG.camera.scroll.y > 0)
+					FlxG.camera.scroll.y -= 20;
+			}
+		}
+	}
+}
+
+class EFPS extends FPS {
+	override public function new() {
+		super(10, 3, FlxColor.WHITE);
+	}
+
+	override public function __enterFrame(deltaTime) {
+		super.__enterFrame(deltaTime);
+
+		textColor = FlxColor.LIME;
+
+		if (currentFPS <= Std.int(Options.framerate / 1.5)) {
+			textColor = FlxColor.YELLOW;
+		}
+		if (currentFPS <= Std.int(Options.framerate / 3.5)) {
+			textColor = FlxColor.RED;
+		}
 	}
 }

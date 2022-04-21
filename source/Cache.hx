26a9189cf2e83aff95505afef51516bde8f0afcb
Paidyy <wieszielonagora2@gmail.com>
2022-04-21 23:20:09 +0200

    add cache for sounds and binarys

--- source/Cache.hx
+++ source/Cache.hx
@@ -1,5 +1,11 @@
 package;
 
+import haxe.Resource;
+import sys.io.File;
+import sys.FileSystem;
+import haxe.io.Bytes;
+import openfl.media.Sound;
+
 /**
  * Class that caches game assets
  */
@@ -8,6 +14,10 @@ class Cache {
 	public static var bfs:Map<String, Boyfriend> = new Map<String, Boyfriend>();
 
     public static var stages:Map<String, Stage> = new Map<String, Stage>();
+
+    public static var sounds:Map<String, Sound> = new Map<String, Sound>();
+
+    public static var bytes:Map<String, Bytes> = new Map<String, Bytes>();
 
     public static function cacheCharacter(char, daChar) {
 		if (char == "bf") {
@@ -26,6 +36,18 @@ class Cache {
     public static function cacheStage(name) {
         if (!Cache.stages.exists(name)) {
             Cache.stages.set(name, new Stage(name));
+        }
+    }
+
+	public static function cacheSound(path:String) {
+        if (!Cache.sounds.exists(path)) {
+            Cache.sounds.set(path, Sound.fromFile(path));
+        }
+    }
+
+    public static function cacheBytes(path:String) {
+        if (!Cache.bytes.exists(path)) {
+            Cache.bytes.set(path, File.getBytes(path));
         }
     }
 }
